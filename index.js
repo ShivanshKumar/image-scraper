19bd246019cbfe6079c19dbe26405b008c32963f
Shivansh <shivanshkumar693@gmail.com>
2021-05-19 21:52:43 +0530

    Preparing for heroku deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const scraper = require("images-scraper");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT||8000;
 
 app.use(express.json())
 
@@ -11,6 +11,11 @@ const google = new scraper({
         headless: false,
     },
 });
+
+app.get('/',(req,res)=>{
+    res.send("Try POST requests")
+})
+
 
 app.post('/', (req,res)=>{
     const scrapeImages = async () =>{
